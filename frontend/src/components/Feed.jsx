5cf6ed272f2f72c1934c7c78d4aaa599eb1abe5f
riyaaa747 <riyaaasings@gmail.com>
2026-07-08 23:44:53 +0530

    home page and manage feed of user

---
+++ frontend/src/components/Feed.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Posts from './Posts'
+
+const Feed = () => {
+  return (
+    <div className='flex-1 my-4 px-2 sm:px-4 flex flex-col '>
+        <Posts/>
+    </div>
+  )
+}
+
+export default Feed
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Feed from './Feed'
+import { Outlet } from 'react-router-dom'
+import RightSidebar from './RightSidebar'
+import useGetAllPost from '@/hooks/useGetAllPost'
+import useGetSuggestedUsers from '@/hooks/useGetSuggestedUsers'
+
+const Home = () => {
+    useGetAllPost();
+    useGetSuggestedUsers();
+    return (
+        <div className='flex'>
+            <div className='flex-grow'>
+                <Feed />
+                <Outlet />
+            </div>
+            <div  className='hidden lg:block'>
+            <RightSidebar />
+            </div>
+        
+        </div>
+    )
+}
+
+export default Home
